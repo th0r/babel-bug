4509981c8231af0cd32eb76578a9ca04a23d9b9c
th0r <grunin.ya@ya.ru>
2015-02-28 12:45:08 +0300

    Replace `add` method with `set`

--- bug.js
+++ bug.js
@@ -1,7 +1,4 @@
 var map = new Map();
 
 // throws "undefined is not a function" in every node.js or io.js
-map.add('key', 'val');
-
-// throws "undefined is not a function" in every node.js or io.js
 console.log(map.values());
